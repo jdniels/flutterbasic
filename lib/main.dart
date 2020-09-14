f9e2687a50e997767d64dc43e4db5435762759e3
Juan Daniel <jdniels525@gmail.com>
2020-09-15 01:05:55 +0700

    learn state full widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() {runApp(MyApp());}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  int num = 0;
+
+  void counter(){
+    setState(() {
+      num +=1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter Basic Day 1"),
-        ),
+        appBar: AppBar(title: Text("State Full WidgetCounter"),),
         body: Center(
-            child: Container(
-                color: Colors.red,
-                height: 50,
-                width: 150,
-                child: Text(
-                  "Day 1 : Learn basic flutter",
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ))),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(num.toString()),
+              RaisedButton(
+                child: Text("Counter"),
+                onPressed: counter,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
